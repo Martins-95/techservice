24c8251164002b6e478f64092b3ec13a3bb37499
Martins-95 <joopedro.martins6@gmail.com>
2026-07-31 16:29:36 +0100

    Modos de execução
    Aleterei o main.py para rodar em modo cli ou interface gráfica(Flet)

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import flet as ft
-from src.views.cliente_view import ClienteView
+def rodar_terminal():
+    from src.views.menu import exibir_menu
+    exibir_menu()
 
+def rodar_flet():
+    import flet as ft
+    from src.views.cliente_view import ClienteView
 
-def main(page: ft.Page):
-    page.title = "TechService - Sistema de Gestão de Assistência Técnica"
-    page.window_width = 850
-    page.window_height = 700
-    page.padding = 20
+    def main_gui(page: ft.Page):
+        page.title = "TechService - Sistema de Gestão"
+        page.window_width = 850
+        page.window_height = 700
+        page.padding = 20
 
-    # Instancia a vista sem passar 'page'
-    cliente_view = ClienteView()
-    page.add(cliente_view)
+        cliente_view = ClienteView()
+        page.add(cliente_view)
 
+    ft.app(target=main_gui)
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    print("\n=== TECHSERVICE - MODO DE EXECUÇÃO ===")
+    print("1. Modo Terminal (Consola / CLI)")
+    print("2. Modo Interface Gráfica (Flet)")
+    
+    opcao = input("Como deseja iniciar o programa? (1 ou 2): ").strip()
 
+    if opcao == "2":
+        rodar_flet()
+    else:
+        rodar_terminal()
